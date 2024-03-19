354091f83580a48fa2ae8c8ac6ebddc0d296f1b3
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2024-03-19 14:18:04 +0200

    Added method to connect harmonyDB.

--- db/sisConnectDB.py
+++ db/sisConnectDB.py
@@ -1,14 +1,27 @@
-from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE
+import config
 import pymysql.cursors
 
 
 def get_connection():
     connection = pymysql.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_DATABASE,
+        host=config.DB_HOST,
+        port=config.DB_PORT,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        database=config.DB_DATABASE,
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+    return connection
+
+
+def get_harmony_connection():
+    connection = pymysql.connect(
+        host=config.H_DB_HOST,
+        port=config.H_DB_PORT,
+        user=config.H_DB_USER,
+        password=config.H_DB_PASSWORD,
+        database=config.H_DB_DATABASE,
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor,
     )
