a6cb9fc281b7aa5dcafd40fe975a105f337bdf50
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2024-03-26 11:12:34 +0200

    Added/updated functions to verify user token

--- helpers/jwt_token.py
+++ helpers/jwt_token.py
@@ -1,5 +1,5 @@
 import jwt
-import db
+import db.dbConnections as db
 
 private_key = open('helpers/keys/es256_private.pem').read()
 public_key = open('helpers/keys/es256_public.pem').read()
@@ -31,7 +31,7 @@ def decode_token(token):
     return jwt.decode(token, public_key, algorithms=['ES256'])
 
 
-def verify_token(token):
+def verify_token(token, user_id):
     """
     Verify the given token
 
@@ -41,39 +41,42 @@ def verify_token(token):
     Returns:
     bool: True if the token is verified, False otherwise
     """
-    try:
-        jwt.decode(token, public_key, algorithms=['ES256'])
+    try:  # try to decode the token
+        decode_token(token)
+        if not db_verify_token(token, user_id):  # check if the token is valid in the database
+            return False
         return True
-    except:
+    except jwt.exceptions.PyJWTError:  # token is invalid
         return False
 
-#
-# def db_verify_token(token, user_id):
-#     '''
-#     Verifies a token
-#
-#     Args:
-#     token: str
-#     user_id: int
-#
-#     Returns:
-#     bool: True if the token is verified, False otherwise
-#     '''
-#     # get a connection to the database
-#     connection = db.get_connection()
-#     try: # try to verify the token
-#         with connection: # use the connection
-#             with connection.cursor() as cursor: # get a cursor
-#                 sql = "SELECT * FROM `tokens` WHERE `token`=%s AND `user_id`=%s AND `valid_until` > NOW()" # SQL query to verify the token
-#                 cursor.execute(sql, (token, user_id)) # execute the query
-#                 result = cursor.fetchone() # get the result
-#                 if result: # check if the token is verified
-#                     if result["revoked_at"]: # check if the token is revoked
-#                         return False
-#                     else:
-#                         return True
-#                 else:
-#                     return False
-#
-#     except Exception as e: # handle exceptions
-#         return str(e) # return the exception as a string
+
+def db_verify_token(token, user_id):
+    """
+    Verifies a token
+
+    Args:
+    token: str
+    user_id: int
+
+    Returns:
+    bool: True if the token is verified, False otherwise
+    """
+    # get a connection to the database
+    connection = db.get_connection()
+    try:  # try to verify the token
+        with connection:  # use the connection
+            with connection.cursor() as cursor:  # get a cursor
+                # SQL query to verify the token
+                sql = "select * from tokens where token = %s and user_id = %s and valid_until > NOW()"
+                cursor.execute(sql, (token, user_id))  # execute the query
+                result = cursor.fetchone()  # get the result
+                if result:  # if the token exists
+                    if result["revoked_at"]:  # check if the token is revoked
+                        return False
+                    else:
+                        return True
+                else:
+                    return False
+
+    except Exception as e:  # handle exceptions
+        return str(e)  # return the exception as a string
