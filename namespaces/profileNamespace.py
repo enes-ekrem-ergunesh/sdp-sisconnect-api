919e4a294450cc9ebb1f3df6ac47d7ad6c50673c
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2025-01-14 19:49:29 +0100

    endpoint to get  profile information using user id

--- namespaces/profileNamespace.py
+++ namespaces/profileNamespace.py
@@ -1,8 +1,12 @@
 from flask import request
 from flask_restx import Namespace, Resource, fields
 import dao.profileDao as profileDao
+import dao.userDao as userDao
+from namespaces.harmonyStudentNamespace import get_student_by_email
+from namespaces.harmonyPersonnelNamespace import get_personnel_by_email
 
 dao = profileDao.ProfileDAO()
+user_dao = userDao.UserDAO()
 
 
 ns = Namespace('profiles', description='Profile related operations')
@@ -16,13 +20,25 @@ profile_info_model = ns.model('Profile Info', {
     'is_admin': fields.Boolean(required=True, description='The user is admin'),
 })
 
-def get_profile_by_user_id(user_id):
-    return dao.get_by_user_id(user_id)
+def collect_profile_info(user_id):
+    _id = dao.get_by_user_id(user_id)['id']
+    email = user_dao.get_by_id(user_id)['email']
+    profile_info = {
+        'id': _id,
+        'user_id': user_id,
+        'email': email,
+        'first_name': get_personnel_by_email(email)['first_name'] if get_personnel_by_email(email) else get_student_by_email(email)['first_name'],
+
+    }
+
+    return profile_info
+
+
 
 @ns.route('/<int:user_id>')
 class ProfileInfo(Resource):
     @ns.doc('get_profile')
     def get(self, user_id):
         """Get profile by user id"""
-        profile_info = get_profile_by_user_id(user_id)
-        return {"message": "getting profile"}
+        profile_info = collect_profile_info(user_id)
+        return {"message": profile_info}
